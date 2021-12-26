1695fa14b3beeabf7a6c1b139605518591c7dad5
Marshall.Miller <marshall.miller@flywire.com>
2021-12-25 22:42:52 -0500

    day20part1

--- day22/include/day22.h
+++ day22/include/day22.h
@@ -9,7 +9,7 @@ typedef struct {
     int x_min, x_max, y_min, y_max, z_min, z_max, on;
 } input_line;
 
-input_line *alloc_input(const char *file, int length);
+input_line *alloc_input(const char *file, int iterations);
 void dealloc_input(input_line *input);
 
 #endif //ADVENT2021_DAY22_H
